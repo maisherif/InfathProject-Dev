405afef1af5abf5421b59ffedbb9b4d8892ae67f
maisherif <37934133+maisherif@users.noreply.github.com>
2023-09-14 12:24:04 +0200

    update

--- src/test/java/ProjectsTests/Addparticiepent.java
+++ src/test/java/ProjectsTests/Addparticiepent.java
@@ -35,47 +35,38 @@ public class Addparticiepent extends TestBase{
 	@Test (dependsOnMethods = {"loginSP"} )
 	public void addpartipenttypeonbehalfofhimself() throws InterruptedException
 	{
+		randomnumber = Integer.toString(int_random);
 		addpartipent = new AddParticipants(driver);	
 		addpartipent.openauctionlist();		
 		addpartipent.gotoparticipenttab();
-//
-//		addpartipent.addpartipent();
-//		addpartipent.addParticipantDetails("1004623136", "partipent1", randomnumber, "1234567893", "mai@hotmail.com");
-//		addpartipent.addtypeCheque("10000", randomnumber);
-//		addpartipent.addtypeTransfer("10000", randomnumber, "SA0410000079451000000207");
-//		addpartipent.addtypeEwallet("10000", randomnumber);
-//		addpartipent.nextbutton();
-//		addpartipent.registerparticipant();
-		
 		addpartipent.addpartipent();
-		addpartipent.addParticipantDetails("1004623128", "partipent1", randomnumber, "1234567893", "mai@hotmail.com");
+		addpartipent.addParticipantDetails("2222222222", "partipent1", randomnumber, "0512345678", "mai@hotmail.com");
 		addpartipent.addtypeCheque("10000", randomnumber);
 		addpartipent.addtypeTransfer("10000", randomnumber, "SA0410000079451000000207");
 		addpartipent.addtypeEwallet("10000", randomnumber);
 		addpartipent.nextbutton();
 		addpartipent.registerparticipant();
-		
 	}
 	
-	@Test (dependsOnMethods = {"loginSP"}, enabled = false )
+	@Test (dependsOnMethods = {"addpartipenttypeonbehalfofhimself"} )
 	public void addpartipenttypeonbehalfofentity() throws InterruptedException
 	{
+		randomnumber = Integer.toString(int_random);
 		addpartipent = new AddParticipants(driver);	
 		
 		addpartipent.openauctionlist();		
 		addpartipent.gotoparticipenttab();
 		addpartipent.addpartipent();
-		addpartipent.addParticipantDetails("1004623128", "partipent2", "2", "1234567893", "mai@hotmail.com");
-		addpartipent.addtypeCheque("10000", "1234");
-		addpartipent.addtypeTransfer("10000", "421", "SA0410000079451000000207");
-		addpartipent.addtypeEwallet("10000", "132");
+		addpartipent.addParticipantDetails("1004933261", "partipent2", "4", "0512345678", "mai@hotmail.com");
+		addpartipent.addtypeCheque("10000", "4");
+		addpartipent.addtypeTransfer("10000", "4", "SA0410000079451000000207");
+		addpartipent.addtypeEwallet("10000", "4");
 		addpartipent.nextbutton();
-	    addpartipent.participanttype("1");
-		addpartipent.typeonbehalfofanentity("7012345678", "namee");
-		addpartipent.ProveEntityAuthorization("1");
+//	    addpartipent.participanttype("1");
+//		addpartipent.typeonbehalfofanentity("7012345678", "namee");
+//		addpartipent.ProveEntityAuthorization("1");
 		addpartipent.registerparticipant();
 	}
-
 
 
 }
